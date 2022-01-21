bf7e72141b872f3a045a4ac285bae042cd83c589
Misael Mendoza <misaelmendozagtz@gmail.com>
2022-01-20 21:30:57 -0700

    First Commit
    Creación de base del Chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { ChatEngine } from "react-chat-engine";
 import './App.css';
+import ChatFeed from './components/ChatFeed.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+        height="100vh"
+        projectID="1c9b1e3f-2d06-4376-bf59-bc64fbaba554"
+        userName="Misael"
+        userSecret="misael"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() => new Audio('https://chat-engine-assets.s3.mazooonaaaaws.com/click.mp3').play()}
+      />
     </div>
   );
 }
